0f7ca53121d57d5ce69642edb80d00b448006697
hazelWolf <bhomitb@outlook.com>
2020-06-12 01:29:09 +0530

    initial

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(gameObject.transform.rotation.x, gameObject.transform.rotation.y - 5, gameObject.transform.rotation.z) * Time.deltaTime);
+    }
+}
